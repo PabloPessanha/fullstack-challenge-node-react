801f2ad59591d10a38d1eaacd16018410d3513cc
Pablo <pablopessanh@gmail.com>
2021-06-24 20:51:55 -0300

    db: initializating db connections; created 'directors' table migration

---
+++ backend/src/database/migrations/1624577641176-CreateDirectors.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateDirectors1624577641176 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'directors',
+        columns: [
+          {
+            name: 'director_id',
+            type: 'integer',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'password_hash',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('directors');
+  }
+}
