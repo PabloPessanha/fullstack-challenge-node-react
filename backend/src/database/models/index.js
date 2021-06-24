8af0508cbf4387e27da05890e71ff5b609f44c3d
Pablo <pablopessanh@gmail.com>
2021-06-24 16:06:08 -0300

    erased all; restarting project

--- backend/src/database/models/index.js
+++
@@ -1,9 +0,0 @@
-const Director = require('./Director');
-const PublicSchool = require('./PublicSchool');
-const Teacher = require('./Teacher');
-
-module.exports = {
-  Director,
-  PublicSchool,
-  Teacher,
-};
