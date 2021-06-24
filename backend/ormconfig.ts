27c4ee7f26ac538a22f015b7ace8dfaba4595cef
Pablo <pablopessanh@gmail.com>
2021-06-24 17:49:37 -0300

    backend: add typeorm; config orm project

---
+++ backend/ormconfig.ts
@@ -0,0 +1,11 @@
+import { config } from 'dotenv';
+
+config();
+
+export default {
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+};
